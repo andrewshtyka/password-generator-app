767ffb02123242bb5bf8a4cc3e2053d6712ff99a
Andrew Shtyka <andrii.shtyka@gmail.com>
2025-09-06 22:18:50 +0300

    test v2

--- src/scripts/animate-cursor.js
+++ src/scripts/animate-cursor.js
@@ -34,23 +34,72 @@ export function animationCard(bgEl, cardEl) {
 }
 
 // replace cursor
-export function animationReplaceCursor(selector) {
-  gsap.set(selector, { xPercent: -50, yPercent: -50, opacity: 0 });
+// export function animationReplaceCursor(selector) {
+//   gsap.set(selector, { xPercent: -50, yPercent: -50, opacity: 0 });
 
-  let xTo = gsap.quickTo(selector, "x", { duration: 0.3, ease: "power4" });
-  let yTo = gsap.quickTo(selector, "y", { duration: 0.3, ease: "power4" });
+//   let xTo = gsap.quickTo(selector, "x", { duration: 0.3, ease: "power4" });
+//   let yTo = gsap.quickTo(selector, "y", { duration: 0.3, ease: "power4" });
 
-  // show cursor after forst move
-  window.addEventListener(
-    "pointermove",
-    (e) => {
-      gsap.set(selector, { x: e.clientX, y: e.clientY, opacity: 1 });
-    },
-    { once: true }
-  );
+//   // show cursor after forst move
+//   window.addEventListener(
+//     "pointermove",
+//     (e) => {
+//       gsap.set(selector, { x: e.clientX, y: e.clientY, opacity: 1 });
+//     },
+//     { once: true }
+//   );
 
-  window.addEventListener("pointermove", (e) => {
-    xTo(e.clientX);
-    yTo(e.clientY);
-  });
+//   window.addEventListener("pointermove", (e) => {
+//     xTo(e.clientX);
+//     yTo(e.clientY);
+//   });
+// }
+
+export function animationReplaceCursor(cursorEl) {
+  if (!cursorEl) return;
+
+  const updateCursorState = () => {
+    if (window.innerWidth >= 1024) {
+      // desktop - custom cursor
+      gsap.set(cursorEl, { opacity: 1, xPercent: -50, yPercent: -50 });
+      document.body.style.cursor = "none";
+
+      // cursor movement
+      const xTo = gsap.quickTo(cursorEl, "x", {
+        duration: 0.3,
+        ease: "power4",
+      });
+      const yTo = gsap.quickTo(cursorEl, "y", {
+        duration: 0.3,
+        ease: "power4",
+      });
+
+      const pointerMoveHandler = (e) => {
+        xTo(e.clientX);
+        yTo(e.clientY);
+      };
+
+      window.addEventListener("pointermove", pointerMoveHandler);
+
+      // save to remove
+      animationReplaceCursor._handler = pointerMoveHandler;
+    } else {
+      // mobile / tablets - default cursor
+      gsap.set(cursorEl, { opacity: 0 });
+      document.body.style.cursor = "auto";
+
+      // remove prev event listener (in case there was one)
+      if (animationReplaceCursor._handler) {
+        window.removeEventListener(
+          "pointermove",
+          animationReplaceCursor._handler
+        );
+        animationReplaceCursor._handler = null;
+      }
+    }
+  };
+
+  // init
+  updateCursorState();
+  window.addEventListener("resize", updateCursorState);
 }
